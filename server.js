691401b7eec1ba94e08401710c70968545a71430
DerrickLui <rick.pantoja@gmlegacyholdings.com>
2026-03-10 10:20:14 +0800

    Upload Genin Pillars 2D

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// SharedArrayBuffer (Godot 4 WASM) headers
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    next();
+});
+
+// Serve static files from 'src'
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Redirect all requests to index.html (SPA support if needed)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+    console.log('Serving from: ' + path.join(__dirname, 'src'));
+});
